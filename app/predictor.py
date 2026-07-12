bfe36bbd2d46697d17617bd3908d1d3e82c1aa8d
BlackCyrus2002 <atchue2002@gmail.com>
2026-07-12 07:54:53 +0000

    Railway Fix

--- app/predictor.py
+++ app/predictor.py
@@ -6,18 +6,27 @@ class PredictionService:
 
     def predict(self, data: dict):
 
+        print("1 - Début prediction")
+
         model = model_loader.get_model()
+
+        print("2 - Modèle chargé")
+
         selected_features = model_loader.get_features()
 
-        if model is None or selected_features is None:
-            raise RuntimeError("Le modèle n'est pas disponible pour le moment.")
+        print("3 - Features chargées")
 
         X = preprocess_input(
             data,
             selected_features
         )
 
+        print("4 - Préprocessing terminé")
+        print(X)
+
         prediction = model.predict(X)
+
+        print("5 - Prediction terminée")
 
         return {
             "next_latitude": float(prediction[0][0]),
